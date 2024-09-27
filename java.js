6fc5735ea6d9953218c41100d9063129bd9e09fd
Qui3tSt0rm <charissahemmer@yahoo.com>
2024-09-26 22:12:47 -0400

    Add files via upload

--- java.js
+++ java.js
@@ -1,10 +1,10 @@
-let humanScore = 0
 
+let humanScore = 0
 let computerScore = 0
 
 const computerChoice = Math.random();
-    
-function getComputerChoice(string) {
+
+function getComputerChoice() {
       if (computerChoice <= 0.33) {
       return "rock";
     } else if (computerChoice >= 0.66) {
@@ -14,11 +14,12 @@ function getComputerChoice(string) {
     }
   }
 
-   // console.log(getComputerChoice('')); //
+let option = window.prompt("Please choose rock, paper, or scissors");
+let humanChoice = option.toLowerCase();
 
-  let option = window.prompt("Please choose rock, paper, or scissors");
 
-  function getHumanChoice(string) {
+
+  function getHumanChoice() {
     if (option === "rock") {
     return "rock";
   } else if (option === "paper") {
@@ -30,47 +31,117 @@ function getComputerChoice(string) {
   }
 }
 
-
-
 let humanSelection = getHumanChoice();
 let computerSelection = getComputerChoice();
 
 
+
 function playRound(humanChoice, computerChoice) {
-    if ((humanSelection) === (computerSelection)) {
-      return "It's a tie!";
-    } else if ((humanSelection = "rock") && (computerSelection = "paper")) {
-      return "You lose, paper beats rock";
-    } else if ((humanSelection = "paper") && (computerSelection = "scissors")) {
-      return "You lose, scissors beats paper";
-    } else if ((humanSelection = "scissors") && (computerSelection = "rock")) {
-      return "You lose, rock beats scissors";
-    } else if ((humanSelection = "rock") && (computerSelection = "scissors")) {
-        return "You win, rock beats scissors";
-    } else if ((humanSelection = "paper") && (computerSelection = "rock"))     {
-        return "You win, paper beats rock";
-    } else if ((humanSelection = "scissors") && (computerSelection = "paper")) {
-        return "You win, scissors beats paper";
+  if ((humanSelection === computerSelection)) {
+    return "It's a tie!";
+  } else if ((humanSelection === "rock") && (computerSelection === "paper")) {
+    result = 'Lose';
+    compWin = ++computerScore;
+    return "You lose, paper beats rock";
+  } else if ((humanSelection === "paper") && (computerSelection === "scissors")) {
+    result = 'Lose';
+    compWin =++computerScore
+    return "You lose, scissors beats paper";
+  } else if ((humanSelection === "scissors") && (computerSelection === "rock")) {
+    result = 'Lose';
+    compWin = ++computerScore;
+    return "You lose, rock beats scissors";
+  } else if ((humanSelection === "rock") && (computerSelection === "scissors")) {
+    result = 'Win';
+    humanWin = ++humanScore;
+    return "You win, rock beats scissors";
+  } else if ((humanSelection === "paper") && (computerSelection === "rock"))  {
+    result = 'Win';
+    humanWin = ++humanScore
+    return "You win, paper beats rock";
+  } else if ((humanSelection === "scissors") && (computerSelection === "paper")) {
+    result = 'Win';
+    humanWin = ++humanScore;
+    return "You win, scissors beats paper";
     }
+    console.log(humanSelection, computerSelection);
+
   }
+
+//playRound(humanSelection, computerSelection);
+  
+   
+
+console.log (humanSelection, computerSelection);
+console.log (playRound(''));
+console.log(humanScore, computerScore);
+  
+  //function playGame (getComputerChoice, getHumanChoice) {
+
+ // playRound(5)
+ // humanChoice(5)
+ // computerChoice(5)
+    
+   //if (humanScore > computerScore) {
+  //  return "You beat the computer!";
+  //} else if (humanScore === computerScore) {
+ //   return "It's a draw!";
+  //} else if (humanScore < computerScore) {
+   // return "The computer beat you!";
+ // }
+
+
+
+// console.log(playGame(''));
+  
+    // playRound(5);  
+
+  //if (humanScore > computerScore);
+   //  return "You beat the computer!";
+   //else if (humanScore === computerScore);
+   // return "It's a draw"
+   // else if (humanScore < comptuterScore);
+   // return ("The computer beat you!");
+
+
+
+     
+    
+    
+
+
+      
+
+
+
+
+
+  
+   //playRound(5);
+ // getHumanChoice(5);
         
 
 
 
 
-console.log (humanSelection, computerSelection);
-console.log (playRound(''));
 
 
 
- //playRound(humanSelection, computerSelection); //
+
+
+
+
+ 
+
+
+
+
 
 
   // console.log(getHumanChoice('')); //
 
-
-    
-    
-  
-
-
+  // need to play 5 rounds
+  // need prompt each round
+  // if comp score > human score after 5 rounds, you lose
+  //if human score > comp score after 5 rounds, you win
+  // if comp score = human score, it's a draw //
